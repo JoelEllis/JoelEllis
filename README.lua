c72a324a62625f632f1b63f9d13723f4788bdb06
Joel Ellis <joel@ellis.link>
2020-07-15 21:36:26 +0100

    Templated README

---
+++ README.lua
@@ -0,0 +1,16 @@
+-- Create template
+local template = require "resty.template"
+local readme_src = template.new "README-source.md"
+
+
+-- DateTime of file generation
+readme_src.datetime = os.date("%Y-%m-%dT%xZ")
+
+-- Last git commit in readme source.
+local f = io.popen("git log -n 1 --pretty=reference -- README-source.md", 'r')
+readme_src.git_last = f:read('*a')
+f:close()
+
+-- Write template ot file.
+local file = io.open("README.md", "w")
+file:write(tostring(readme_src))
